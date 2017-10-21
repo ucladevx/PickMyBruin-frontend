f84aba6320b28e23c9c4a75e215728114d75ca41
Ram Goli <ramsgoli@gmail.com>
2017-10-21 00:03:57 -0700

    add .babel files, builds correctly

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import {Route, Switch, Redirect} from 'react-router-dom';
 import {ConnectedRouter} from 'react-router-redux';
 import {render} from 'react-dom';
 
-import {store, history} from 'reducer';
+import {store, history} from './reducer';
 
 import Navbar from 'view/navbar';
 import Home from 'container/home';
@@ -14,24 +14,26 @@ import About from 'container/about';
 
 
 class App extends React.Component {
-  render(){
-    return <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <div>
-          <Navbar/>
-          <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route path="/about" component={About}/>
-            <Redirect to="/"/>
-          </Switch>
-        </div>
-      </ConnectedRouter>
-    </Provider>;
-  }
+	render() {
+		return(
+		    <Provider store={store}>
+                <ConnectedRouter history={history}>
+                <div>
+                    <Navbar/>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/about" component={About}/>
+                        <Redirect to="/"/>
+                    </Switch>
+                </div>
+                </ConnectedRouter>
+            </Provider>
+        )
+    }
 }
 
 
 render(
-  <App/>,
-  document.getElementById('mount')
+    <App/>,
+    document.getElementById('mount')
 );
