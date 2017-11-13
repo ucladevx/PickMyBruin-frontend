6b5d11824bfcd47c9c2d9308d67062468d198229
Ram Goli <ramsgoli@gmail.com>
2017-11-12 23:02:46 -0800

    make search bar on desktop version rounded

--- src/components/profile/general.js
+++ src/components/profile/general.js
@@ -3,7 +3,7 @@ import React from 'react';
 class General extends React.Component {
     render() {
         return(
-            <div className="middle">
+            <div className="general">
                 <h1>Linguistics</h1>
             </div>
         );
