53b420e1ff04fa67a210e2b89895e745d8591486
Ram Goli <ramsgoli@gmail.com>
2017-12-04 14:52:33 -0800

    splash done

--- src/components/pages/home/alksjfa.js
+++ src/components/pages/home/alksjfa.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {push} from 'react-router-redux';
-import { Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
@@ -10,9 +9,6 @@ class Home extends React.Component {
             <div className="container home">
                 <h1>One-sentence catchline to onboard new users.</h1> 
                 <div className="info">
-                    <Button color="primary" onClick={this.props.signUp} block>Sign Up</Button>
-                    <p>Already have an account?</p>
-                    <Link className="btn btn-primary" to="/login"> Login </Link>
                 </div>
             </div>
         )    
--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 import TopBar from './topBar';
+
+import messages from '../../../../images/landingPage/messages.svg';
 class Home extends React.Component {
     render() {
         return (
             <div className="home-container">
                 <div className="landing-screen">
                     <TopBar />
+                    <div className="splash">
+                        <div className="main-prompt">
+                            <h1>Connect with your fellow Bruins about major-related questions.</h1>
+                            <Button color="primary" onClick={this.props.signUp}>Sign Up</Button>
+                            <p>Already have an account? <Link className="login-link" to="/login">Log in here</Link></p>
+                            
+                        </div>
+                        <img src={messages} alt="messages" />
+                    </div>
                 </div>
                 <div className="explanation">
                     How it works
