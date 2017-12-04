a90a938a596e05ea9c46d45fb62124ad95e3e1ba
Ram Goli <ramsgoli@gmail.com>
2017-12-04 14:33:44 -0800

    topbar

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import logo from '../../../../images/landingPage/logo.png';
 
+import TopBar from './topBar';
 class Home extends React.Component {
     render() {
         return (
             <div className="home-container">
                 <div className="landing-screen">
-                    <section className="top-bar">
-                        <div className="logo">
-                            <img src={logo} id="logo" alt="logo" />
-                        </div>
-                        <div className="nav-buttons">
-                            <div id="about">About DevX</div>
-                            <div id="how">How It Works</div>
-                        </div>
-                    </section>
+                    <TopBar />
                 </div>
                 <div className="explanation">
                     How it works
