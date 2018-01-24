ae502d37e792691018d8154146a05633f1178e3a
kissyourmisery <21330xuchengyin@gmail.com>
2018-01-23 22:34:52 -0800

    profile pic looks nicer now

--- src/components/pages/profile/profilePicture.js
+++ src/components/pages/profile/profilePicture.js
@@ -23,10 +23,11 @@ class ProfilePic extends React.Component {
 	}
 
     render() {
+    	const url = this.props.profile.getIn(['user','picture']) ? this.props.profile.getIn(['user','picture']) : 'http:\/\/www.iconninja.com/files/373/611/612/person-user-profile-male-man-avatar-account-icon.svg'
+
         return(
         	<ReactFileReader fileTypes={[".jpg",".png",".svg",".jif"]} multipleFiles={false} handleFiles={this._handleFiles}>
-	            <div className="profile-pic">
-	                {this.renderProfilePic()}
+	            <div className="profile-pic" style={{backgroundImage: 'url('+url+')'}}>
 	            </div>
 	        </ReactFileReader>
         )
