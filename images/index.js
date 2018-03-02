80082816992226db01a19ca15e64a857adac9c31
Christine <jbcccsun@gmail.com>
2018-03-01 18:15:24 -0800

    favicon (and also hoverable prof pic) (#43)
    * add hover effect to profile picture
    
    * add favicon to pages folder
    
    * typo in index.html
    
    * only editable hovers
    
    * revert package-lock.json

--- images/index.js
+++ images/index.js
@@ -2,3 +2,4 @@ export logo from './landingPage/logo.png';
 export bottom from './landingPage/bottom.svg';
 export explanation from './landingPage/explanation.svg';
 export messages.svg from './landingPage/messages.svg';
+export favicon from './landingPage/favicon-16x16.png';
