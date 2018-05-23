a8c675c8d82a2b558622855428a5219dbb311a44
Ram Goli <ramsgoli@gmail.com>
2018-05-23 16:48:56 -0700

    capitalization

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from '../../util/Button';
 import { Link } from 'react-router-dom';
 
-import TopBar from './TopBar';
+import TopBar from './topBar';
 import bg from '../../../../images/landingPage/bg.svg.inline';
 
 class Home extends React.Component {
