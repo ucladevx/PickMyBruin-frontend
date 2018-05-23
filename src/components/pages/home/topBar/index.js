727508de8ed98bd498bfd3515b2babb550c26df0
Ram Goli <ramsgoli@gmail.com>
2018-05-22 23:12:42 -0700

    landing page background

--- src/components/pages/home/topBar/index.js
+++ src/components/pages/home/topBar/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import logo from '../../../../../images/landingPage/logo.png';
-
 export default (props) => {
     return (
         <section className="top-bar">
-            <div className="logo">
-                <img src={logo} id="logo" alt="logo" />
-            </div>
             <div className="nav-buttons">
                 <Link id="login" to="/login">Login</Link>
                 <Link id="signup" to="/register">Sign Up</Link>
