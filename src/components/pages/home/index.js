8416157624d60e085545b1c3384d41b584cf2b62
Ram Goli <ramsgoli@gmail.com>
2018-05-23 00:07:57 -0700

    done

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from '../../util/Button';
 import { Link } from 'react-router-dom';
 
+import TopBar from './TopBar';
 import bg from '../../../../images/landingPage/bg.svg.inline';
 
 class Home extends React.Component {
@@ -10,6 +11,7 @@ class Home extends React.Component {
         return (
             <div className="home-container">
                 <div className="landing-screen">
+                    <TopBar />
                     <div className="bg" dangerouslySetInnerHTML={{__html: bg}} />
                 </div>
             </div>
--- src/components/pages/home/topBar/index.js
+++ src/components/pages/home/topBar/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import newBquestLogo from '../../../../../images/loginPage/teal-bquest-logo.svg.inline';
+import newBquest from '../../../../../images/loginPage/teal-bquest.svg.inline';
+
 export default (props) => {
     return (
         <section className="top-bar">
+            <div className="logo">
+                <div className="logo-logo" dangerouslySetInnerHTML={{__html: newBquestLogo}} />
+                <div className="logo-text" dangerouslySetInnerHTML={{__html: newBquest}} />
+            </div>
             <div className="nav-buttons">
                 <Link id="login" to="/login">Login</Link>
                 <Link id="signup" to="/register">Sign Up</Link>
