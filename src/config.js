1d933aa2cad920c2274643ed4084d95d9a4680fa
Ram Goli <ramsgoli@gmail.com>
2018-05-30 21:05:46 -0700

    fix websockets url

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ const development = process.env.WEBPACK;
 
 export default {
     API_URL: development ? "http://localhost:8000" : "/api",
-    WS_URL: development ? "ws://localhost:8001/listen" : "/api/ws/listen",
+    WS_URL: development ? "ws://localhost:8001/listen" : "wss://bquest.ucladevx.com/api/ws/listen",
     CLIENT_SECRET: "sMXrq4RV2muu07ERhrllW1uCfH12U4ZNeqyM0L8bmaF9P59prjkIe5mhJJAt47Kod14yhRdg96gEf1m183sblRzyC175eLd7NzsyWS9w6QTyVPdczvTmeHiGBJJPjhGf"
 }
