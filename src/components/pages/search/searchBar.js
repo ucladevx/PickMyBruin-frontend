b4edb219ef93e8649f8b622fded61185f03954a2
jhan25 <justinhan1997@gmail.com>
2019-05-27 16:57:45 -0700

    fixed handlesearch and cleaned code

--- src/components/pages/search/searchBar.js
+++ src/components/pages/search/searchBar.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { PropTypes } from 'react';
 
 class SearchBar extends React.Component {
-   
     state = {
         value : "",
-           name: false,
-            year: false,
-            major: false,
-            mentor: false,
+        name: false,
+        year: false,
+        major: false,
+        mentor: false,
     }
   
     submit = e => {
@@ -19,88 +18,53 @@ class SearchBar extends React.Component {
         const majorString = this.state.major ? 'true' : 'false';
         const mentorString = this.state.mentor ? 'true' : 'false';
 
-        // console.log(nameString);
-        // console.log(yearString);
-        // console.log(majorString);
-        // console.log(mentorString);
-
         const URL=`${this.state.value}&name=${nameString}&year=${yearString}&major=${majorString}&mentor=${mentorString}`;
+
         console.log(URL);
+
         this.props.handleSearch(URL);
     }
     
-   handleChange = e => {
-       e.preventDefault();
-       this.setState ({
-           value: e.target.value,
-       })
-   }
+    handleChange = e => {
+        e.preventDefault();
 
-   handleChangeName = e => {
-       e.preventDefault();
+        this.setState ({
+            value: e.target.value,
+        })
+    }
 
-       this.setState({
-           name: e.target.value,
+    handleChangeName = e => {
+        e.preventDefault();
 
-       })
-       // console.log(e.target.value);
-   }
+        this.setState({
+            name: e.target.value,
 
-   handleChangeYear = e => {
-       e.preventDefault();
+        })
+    }
 
-       this.setState ({
-           year: e.target.value,
-       })
-       // console.log(e.target.value);
-   }
+    handleChangeYear = e => {
+        e.preventDefault();
 
-   handleChangeMajor = e => {
-       e.preventDefault();
+        this.setState ({
+            year: e.target.value,
+        })
+    }
 
-       this.setState ({
-           major: e.target.value, 
-       })
-       // console.log(e.target.value);
-   }
+    handleChangeMajor = e => {
+        e.preventDefault();
 
-   handleChangeMentor = e => {
-       e.preventDefault();
-       this.setState ({
-           mentor: e.target.value,
-       })
-       // console.log(e.target.value);
-   }
+        this.setState ({
+            major: e.target.value, 
+        })
+    }
 
+    handleChangeMentor = e => {
+        e.preventDefault();
 
-   // set up handle change for each parameter
-   // set up appropriate string variables based on the boolean values
-
-   // handleChange(e) {
-
-   //      e.preventDefault
-   //      const { name, value } = e.target;
-
-   //      this.setState({
-   //        [name]: value
-   //      });
-
-        
-     
-   //      const nameSelected = this.state.name ? 'true' : false;
-   //      const yearSelected = this.state.year ? true : false;
-   //      const majorSelected = this.state.major ? true : false;
-   //      const mentorSelected = this.state.mentor ? true : false;
-
-   //      const URL=`${value}&name=${nameSelected}&year=${yearSelected}&major=${majorSelected}&mentor=${mentorSelected}`;
-        
-   //      this.setState({ searchURL: URL});
-
-    
-   //  }
-
-   // check functionality of the radio buttons
-   // make use of console.log(variable) to test boolean values
+        this.setState ({
+            mentor: e.target.value,
+        })
+    }
 
     render() {
         return (
@@ -111,24 +75,19 @@ class SearchBar extends React.Component {
                         type="text" 
                         placeholder="Search name, major, ..." 
                         value={this.state.value}
-                        
-                        // track change for query only
-                        onChange={(e) => this.handleChange(e)}
+                        onChange={this.handleChange}
                         required
-                    />
-                                    
+                    />                 
                 </form>
-                <div className="filter" style={{display: 'flex', 
-                  justifyContent: 'center'}}>
-                        <input type="radio" value="name" name="name" onChange={this.handleChangeName}/> Name
-                        <input type="radio" value="year" name="year" onChange={this.handleChangeYear}/> Year
-                        <input type="radio" value="major" name="major" onChange={this.handleChangeMajor}/> Major
-                        <input type="radio" value="mentor" name="mentor" onChange={this.handleChangeMentor}/> Mentor
+                <div className="filter" style={{display: 'flex', justifyContent: 'center'}}>
+                    <input type="radio" value={this.state.name} name="name" onChange={this.handleChangeName}/> Name
+                    <input type="radio" value={this.state.year} name="year" onChange={this.handleChangeYear}/> Year
+                    <input type="radio" value={this.state.major} name="major" onChange={this.handleChangeMajor}/> Major
+                    <input type="radio" value={this.state.mentor} name="mentor" onChange={this.handleChangeMentor}/> Mentor
                 </div> 
-
-
             </div>
         );
     }
 }
+
 export default SearchBar;
--- src/container/search.js
+++ src/container/search.js
@@ -5,16 +5,13 @@ import { Actions } from '../reducer';
 import Search from '../components/pages/search';
 
 class SearchContainer extends React.Component {
-    
     render() {
         return (
             <Search 
                 mentors={this.props.mentors}
-                handleSearch={this.props.handleSearch(this.props.query)}
+                handleSearch={this.props.handleSearch}
                 count={this.props.count}
                 _internal={this.props._internal}
-                query={this.props.searchURL}
-                
             />
         );
     }
@@ -26,13 +23,12 @@ const mapStateToProps = state => {
         mentors: SearchMentors.get('mentors'),
         count: SearchMentors.get('count'),
         _internal: SearchMentors.get('_internal'),
-        query: state.searchURL
     };
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        handleSearch: (query) => {
+        handleSearch: query => {
             dispatch(Actions.searchMentorsActions.handleSearch(query));
         }
     }
