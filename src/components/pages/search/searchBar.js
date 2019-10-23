a6aaece0a02e339c07d2a36a2c1ccce7754609d5
jhan25 <justinhan1997@gmail.com>
2019-10-22 17:20:55 -0700

    cleaned up + fixed search filter

--- src/components/pages/search/searchBar.js
+++ src/components/pages/search/searchBar.js
@@ -1,41 +1,24 @@
 import React from 'react';
-import { PropTypes } from 'react';
-// import ToggleButton from 'react-bootstrap/ToggleButton';
-// import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 import { Component } from 'react';
 import { Button, ButtonGroup } from 'reactstrap';
 
-class SearchBar extends React.Component {
-
-    // constructor(props) {
-    //     super(props);
-    //     this.handleChange = this.handleChange.bind(this);
-    // }
-    constructor (props) {
-    super(props);
-
-    this.state = { cSelected: [],
-                    value : "",
+class SearchBar extends Component {
+    state = {
+        cSelected: [],
+        value : "",
         name: false,
-        year: false,
         major: false,
-        mentor: false
-
-     };
-
-    this.onCheckboxBtnClick = this.onCheckboxBtnClick.bind(this);
-     }
-    
+        bio: false
+    }
   
     submit = e => {
         e.preventDefault();
 
-        const nameString = this.state.name ? 'true' : 'false';
-        const yearString = this.state.year ? 'true' : 'false';
-        const majorString = this.state.major ? 'true' : 'false';
-        const mentorString = this.state.mentor ? 'true' : 'false';
+        const nameString = this.state.name ? '&name=True' : '';
+        const majorString = this.state.major ? '&major=True' : '';
+        const bioString = this.state.bio ? '&bio=True' : '';
 
-        const URL=`${this.state.value}&name=${nameString}&year=${yearString}&major=${majorString}&mentor=${mentorString}`;
+        const URL=`${this.state.value}${nameString}${majorString}${bioString}`;
 
         console.log(URL);
 
@@ -45,77 +28,35 @@ class SearchBar extends React.Component {
     handleChange = e => {
         e.preventDefault();
 
-        this.setState ({
-            value: e.target.value,
-        })
-    }
-
-    handleChangeName = e => {
-        e.preventDefault();
-
         this.setState({
-            name: true,
-
-        })
-        this.setState({ cSelected: [...this.state.cSelected] });
-
+            value: e.target.value,
+        });
     }
 
-    handleChangeYear = e => {
-        e.preventDefault();
+    onCheckboxBtnClick = selected => {
+        var index = this.state.cSelected.indexOf(selected);
+        var newSelected = [...this.state.cSelected];
 
-        this.setState ({
-            year: true,
-        })
-        this.setState({ cSelected: [...this.state.cSelected] });
-
-    }
-
-    handleChangeMajor = e => {
-        e.preventDefault();
-
-        this.setState ({
-            major: true, 
-        })
-        this.setState({ cSelected: [...this.state.cSelected] });
-
-    }
-
-    handleChangeMentor = e => {
-        e.preventDefault();
-
-        this.setState ({
-            mentor: true,
-        })
-        this.setState({ cSelected: [...this.state.cSelected] });
-
-    }
-
-    onCheckboxBtnClick(selected) {
-        const index = this.state.cSelected.indexOf(selected);
         if (index < 0) {
-          this.state.cSelected.push(selected);
+          newSelected.push(selected);
         } else {
-          this.state.cSelected.splice(index, 1);
+          newSelected.splice(index, 1);
         }
-        this.setState({ cSelected: [...this.state.cSelected] });
+
+        this.setState({ cSelected: [...newSelected] });
+
         switch (selected) {
             case 1:
-                this.setState ({name: !this.state.name});
+                this.setState({ name: !this.state.name });
                 break;
             case 2:
-                this.setState ({year: !this.state.year});
+                this.setState({ major: !this.state.major });
                 break;
             case 3:
-                this.setState ({major: !this.state.major});
-                break;
-
-            case 4:
-                this.setState ({mentor: !this.state.mentor});
+                this.setState({ bio: !this.state.bio });
                 break;
             default:
                 break;
-
         }
       }
 
@@ -127,7 +68,7 @@ class SearchBar extends React.Component {
                         <input 
                             className="field--input" 
                             type="text" 
-                            placeholder="Search name, major, ..." 
+                            placeholder="Search by name, major, or bio..." 
                             value={this.state.value}
                             onChange={this.handleChange}
                             required
@@ -139,16 +80,12 @@ class SearchBar extends React.Component {
                 <p style={{fontWeight: 'bold', display: 'flex', justifyContent: 'center'}}>Filter by: </p>
                 <br></br>
                 <div className="filter" style={{display: 'flex', justifyContent: 'center'}}>
-                      
                     <ButtonGroup>
                       <Button outline color="info" onClick={() => this.onCheckboxBtnClick(1)} active={this.state.cSelected.includes(1)}>Name</Button>
-                      <Button outline color="info" onClick={() => this.onCheckboxBtnClick(2)} active={this.state.cSelected.includes(2)}>Year</Button>
-                      <Button outline color="info" onClick={() => this.onCheckboxBtnClick(3)} active={this.state.cSelected.includes(3)}> Major</Button>
-                      <Button outline color="info" onClick={() => this.onCheckboxBtnClick(4)} active={this.state.cSelected.includes(4)}>Mentor</Button>
+                      <Button outline color="info" onClick={() => this.onCheckboxBtnClick(2)} active={this.state.cSelected.includes(2)}>Major</Button>
+                      <Button outline color="info" onClick={() => this.onCheckboxBtnClick(3)} active={this.state.cSelected.includes(3)}> Bio</Button>
                     </ButtonGroup>
                 </div> 
-                
-
             </div>
         );
     }
