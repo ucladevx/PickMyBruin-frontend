ba1f2d74c0ca71ef808e71d9a4901f71a2922771
Arek Der-Sarkissian <areksds@gmail.com>
2020-04-30 11:58:13 -0700

    Add Logo to Email Pending, Forgot & Password

--- src/components/pages/forget-password/email-pending.js
+++ src/components/pages/forget-password/email-pending.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import newBquest from '../../../../images/loginPage/white-bquest.svg.inline'
-import newBquestLogo from '../../../../images/loginPage/white-bquest-logo.svg.inline'
+import Logo from '../../util/Logo';
 import Footer from '../../../components/footer';
 
 class ForgetPasswordPending extends React.Component {
     render() {
         return (
             <div className="container-login">
-                <div className="logo">
-                    <div className="logo-logo" dangerouslySetInnerHTML={{__html: newBquestLogo}} />
-                    <div className="logo-text" dangerouslySetInnerHTML={{__html: newBquest}} />
-                </div>
+                <Logo color="white" LoggedIn={false} />
                 <div className="container-pending">
                     <h1 className="loginform-title">Email Sent! Check your email :)</h1>
                 </div>
--- src/components/pages/forget-password/index.js
+++ src/components/pages/forget-password/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import newBquest from '../../../../images/loginPage/white-bquest.svg.inline'
-import newBquestLogo from '../../../../images/loginPage/white-bquest-logo.svg.inline'
 import Footer from '../../../components/footer';
+import Logo from '../../util/Logo';
 import Form from '../../util/ForgetPasswordForm';
 
 class ForgetPassword extends React.Component {
     render() {
         return (
             <div className="container-login">
-                <div className="logo">
-                    <div className="logo-logo" dangerouslySetInnerHTML={{__html: newBquestLogo}} />
-                    <div className="logo-text" dangerouslySetInnerHTML={{__html: newBquest}} />
-                </div>
+                <Logo color="white" LoggedIn={false} />
                 <div className="container-password-reset">
                     <h1 className="password-reset-title">Password Reset</h1>
                     <p>We'll send a password reset link to your email.</p>
