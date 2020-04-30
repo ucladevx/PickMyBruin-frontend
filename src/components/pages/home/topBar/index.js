6a258cbe598b132e12e661ee4a4fea58b460af3c
Arek Der-Sarkissian <areksds@gmail.com>
2020-04-29 21:18:02 -0700

    Add util/Logo, update topBar
    Created new class to hold clickable logo and added it to topBar

--- src/components/pages/home/topBar/index.js
+++ src/components/pages/home/topBar/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../../../util/Logo';
 
 import newBquestLogo from '../../../../../images/loginPage/teal-bquest-logo.svg.inline';
 import newBquest from '../../../../../images/loginPage/teal-bquest.svg.inline';
@@ -7,15 +8,10 @@ import newBquest from '../../../../../images/loginPage/teal-bquest.svg.inline';
 export default (props) => {
     return (
         <section className="top-bar">
-            <div className="logo">
-                <div className="logo-logo" dangerouslySetInnerHTML={{__html: newBquestLogo}} />
-                <div className="logo-text" dangerouslySetInnerHTML={{__html: newBquest}} />
-            </div>
+            <Logo color="teal" LoggedIn={props.isLoggedIn} />
             <div className="nav-buttons">
                 <Link id="login" to="/login">Login</Link>
                 <Link id="signup" to="/register">Sign Up</Link>
-                {/*<div id="login" onClick={() => location.href="/login"}>Login</div>*/}
-                {/*<div id="signup" onClick={() => location.href="/register"}>Sign Up</div>*/}
             </div>
         </section>
     );
---
+++ src/components/util/Logo/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { push } from 'react-router-redux';
+import whiteBquest from '../../../../images/loginPage/white-bquest.svg.inline';
+import whiteBquestLogo from '../../../../images/loginPage/white-bquest-logo.svg.inline';
+import tealBquestLogo from '../../../../images/loginPage/teal-bquest-logo.svg.inline';
+import tealBquest from '../../../../images/loginPage/teal-bquest.svg.inline';
+
+class Logo extends React.Component {
+	render() {
+	    return (
+	    	<div>
+		    	{(this.props.color === "teal") && 
+		    	<div className="logo" onClick={() => location.href=(this.props.LoggedIn ? "/home" : "/")} style={{cursor:'pointer'}}>
+		            <div className="logo-logo" dangerouslySetInnerHTML={{__html: tealBquestLogo}} />
+		            <div className="logo-text" dangerouslySetInnerHTML={{__html: tealBquest}} />
+		        </div>
+		    	}
+		        {(this.props.color === "white") && 
+		        <div className="logo" onClick={() => location.href=(this.props.LoggedIn ? "/home" : "/")} style={{cursor:'pointer'}}>
+		            <div className="logo-logo" dangerouslySetInnerHTML={{__html: whiteBquestLogo}} />
+		            <div className="logo-text" dangerouslySetInnerHTML={{__html: whiteBquest}} />
+		        </div>
+		    	}
+			</div>
+	    );
+	}
+}
+
+export default Logo;
